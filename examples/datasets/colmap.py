d86772be01cf739c58972d6e9b4fa82422f5b8bd
Michael Ingerman <michael.ingerman@havocai.com>
2025-08-28 21:08:38 -0400

    wip

--- examples/datasets/colmap.py
+++ examples/datasets/colmap.py
@@ -196,7 +196,7 @@ class Parser:
             )
             image_files = sorted(_get_rel_paths(image_dir))
         colmap_to_image = dict(zip(colmap_files, image_files))
-        image_paths = [os.path.join(image_dir, colmap_to_image[f]) for f in image_names]
+        image_paths = [os.path.join(image_dir, f) for f in colmap_files]
 
         # 3D points and {image_name -> [point_idx]}
         points = manager.points3D.astype(np.float32)
